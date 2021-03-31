6871dc5af04ffb3a72de05a2539e911368897159
Lawrence Yuen <lwrnceyuen@gmail.com>
2021-03-31 00:48:13 -0700

    redesign html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,37 @@
-var city="";
+let city="";
 
-var searchedCity = $("#city-input");
-var searchButton = $("#search-button");
-var clearButton = $("#clear-history");
-var currentCity = $("#current-city");
-var currentTemp = $("#temperature");
-var currentHumidity= $("#humidity");
-var currentWind=$("#wind-speed");
-var currentUVindex= $("#uv-index");
-var sCity=[];
+const cityInputEl = $("#city-input");
+const searchButton = $("#search-button");
+const clearButton = $("#clear-history");
+const currentCity = $("#current-city");
+const currentCondition = $('current-condition')
+const currentTemp = $("#temperature");
+const currentHumidity= $("#humidity");
+const currentWind=$("#wind-speed");
+const currentUVindex= $("#uv-index");
+let sCity=[];
 
-var APIKey="1876ed65ab3cdf8b7a272096e11fe561";
+const APIKey="1876ed65ab3cdf8b7a272096e11fe561";
+
+let formSubmitHandler = function(event) {
+    event.preventDefault();
+    let cityInput = cityInputEl.value.trim();
+
+    if(city) {
+        getWeather(city);
+
+
+    }
+
+}
+
+function currentWeather(city) {
+    let apiURL = "api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
+
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+
+};
